3cb340b437e294755f5c1b9eae8ff88d0b0fc13d
Travis Martin <tjmartin2003@gmail.com>
2020-02-23 23:45:47 -0600

    I've been overcomplicating this a bit whoops.

--- tokenApp/routes/token.js
+++ tokenApp/routes/token.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const request = require("request");
+
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", function(req, res, next) { res.render("token", { token: req.query.token }); });
+router.get("/", function(req, res, next) { 
+	const options = {
+		method: "GET",
+		url: "https://api.twitch.tv/helix/users/",
+		qs: { login: "t3lacuna" },
+		headers: { "Client-ID": "2be6yrftbjgdrs0hov5lhz3uszezzv" }
+	};
+
+	request(options, function(error, response, body) {
+		if (error) { throw new Error(error); }
+		console.log(JSON.parse(body));
+	});
+
+	res.render("token");
+});
 
 module.exports = router;
