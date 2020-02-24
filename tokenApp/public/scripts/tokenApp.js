80f5b632ecfb3e50e96cb6c5838f26e3949891dd
Travis Martin <tjmartin2003@gmail.com>
2020-02-23 22:52:12 -0600

    Working token downloader. Try redirect to handler page next.

--- tokenApp/public/scripts/tokenApp.js
+++ tokenApp/public/scripts/tokenApp.js
@@ -2,7 +2,7 @@ function downloadToken() {
 	// Create a link and generate the text file to download.
 	const token = document.createElement("a");
 	token.style.display = 'none';
-	token.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(window.location.hash));
+	token.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(window.location.hash).match("access_token%3D(.*)%26scope")[1]);
 	token.setAttribute('download', 'token.txt');
 
 	// Simulate clicking the element to activate the download.
