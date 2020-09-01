03785c789878d730cb81d04f12e3908d65a5494f
Travis Martin <tjmartin2003@gmail.com>
2020-09-01 16:27:38 -0500

    Move output directory to match new ~/.t3 format.

--- index.js
+++ index.js
@@ -9,28 +9,41 @@ const os = require('os'); // Get special directories.
 const request = require('request'); // Get data from Twitch API.
 const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout }); // Get input from the user.
 
-// Load variables.
-dotenv.config();
+// Constants
+const PORT = 4213;
+const CLIENT_ID = '2be6yrftbjgdrs0hov5lhz3uszezzv';
+
+// Variables.
 let oauth = null;
 let markKey = null;
 let stopKey = null;
 let user = null;
 let userId = null;
 let streamStartTime = null;
-const dataPath = os.homedir() + process.env.DATA_PATH;
-if (!fs.existsSync(dataPath)) { fs.mkdirSync(dataPath, { recursive: true }); }
-if (fs.existsSync(dataPath + process.env.KEYBIND_FILE)) {
-	const data = JSON.parse(fs.readFileSync(dataPath + process.env.KEYBIND_FILE));
+
+// Directories and files.
+const dataPath = `${os.homedir()}/.t3/StreamMarker/`;
+const outPath = `${dataPath}/out/`;
+const keybindFile = `${dataPath}keybinds.json`;
+const userFile = `${dataPath}user.json`;
+const outFile = `${outPath}${new Date().toISOString().replace(/:/g, '')}.txt`;
+
+// Make directories.
+if (!fs.existsSync(outPath)) { fs.mkdirSync(outPath, { recursive: true }); }
+
+// Load variables.
+if (fs.existsSync(keybindFile)) {
+	const data = JSON.parse(fs.readFileSync(keybindFile));
 	markKey = data.markKey;
 	stopKey = data.stopKey;
 }
-if (fs.existsSync(dataPath + process.env.USER_FILE)) {
-	user = JSON.parse(fs.readFileSync(dataPath + process.env.USER_FILE)).user;
+if (fs.existsSync(userFile)) {
+	user = JSON.parse(fs.readFileSync(userFile)).user;
 	start();
 } else {
 	readline.question('Twitch login name: ', loginName => {
 		user = loginName;
-		fs.writeFile(dataPath + process.env.USER_FILE, JSON.stringify({ user: user }), error => {
+		fs.writeFile(userFile, JSON.stringify({ user: user }), error => {
 			if (error) { console.log(error); }
 		});
 		readline.close();
@@ -38,46 +51,47 @@ if (fs.existsSync(dataPath + process.env.USER_FILE)) {
 	});
 }
 
+// Start program.
 function start() {
 	// Create Express app to get OAuth token.
 	const app = express();
-	app.get('/', (req, res) => res.sendFile(path.join(__dirname + '\\index.html')));
+	app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}\\index.html`)));
 	app.get('/token', (req, res) => {
 		oauth = req.query.token;
 		request({
-			url: process.env.USER_ID_REQUEST + user,
+			url: `https://api.twitch.tv/helix/users?login=${user}`,
 			headers: {
-				'Client-ID': process.env.CLIENT_ID,
-				'Authorization': 'Bearer ' + oauth
+				'Client-ID': CLIENT_ID,
+				'Authorization': `Bearer ${oauth}`
 			}
 		}, (err, response, body) => {
 			userId = JSON.parse(body).data[0].id;
-			console.log('User: ' + user + ' - ' + userId);
+			console.log(`User: ${user} - ${userId}`);
 
 			getStartTime();
 		});
 
-		res.sendFile(path.join(__dirname + '\\token.html'));
+		res.sendFile(path.join(`${__dirname}\\token.html`));
 		app.tokenServer.close();
 	});
-	app.tokenServer = app.listen(process.env.PORT, () => console.log('Now listening on port ', process.env.PORT));
+	app.tokenServer = app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 
 	// Open OAuth request page in browser.
-	open(process.env.AUTH_URL + '?client_id=' + process.env.CLIENT_ID + '&redirect_uri=' + process.env.REDIRECT_URI + '&response_type=token&scope=');
+	open(`https://id.twitch.tv/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=http://localhost:${PORT}&response_type=token&scope=`);
 }
 
 function getStartTime() {
 	const timer = setInterval(() => {
 		request({
-			url: process.env.STREAM_REQUEST + userId,
+			url: `https://api.twitch.tv/helix/streams?user_id=${userId}`,
 			headers: {
-				'Client-ID': process.env.CLIENT_ID,
-				'Authorization': 'Bearer ' + oauth
+				'Client-ID': CLIENT_ID,
+				'Authorization': `Bearer ${oauth}`
 			}
 		}, (err, response, body) => {
 			try {
 				streamStartTime = new Date(JSON.parse(body).data[0].started_at.slice(0, -1));
-				console.log('Stream start time:', streamStartTime.toISOString());
+				console.log(`Stream start time: ${streamStartTime.toISOString()}`);
 				clearInterval(timer);
 				startHook();
 			} catch (error) {
@@ -90,23 +104,23 @@ function getStartTime() {
 // Start hook.
 function startHook() {
 	iohook.start();
-	if (!markKey) { console.log('Please press a key to set it as your mark key.'); } else { console.log('Mark key:', markKey, '\nStop key:', stopKey); }
+	if (!markKey) { console.log('Please press a key to set it as your mark key.'); } else { console.log(`Mark key: ${markKey}\nStop key: ${stopKey}`); }
 	iohook.on('keydown', event => {
 		if (!markKey) {
 			markKey = event.rawcode;
-			console.log('Mark key set:', markKey);
+			console.log(`Mark key set: ${markKey}`);
 			console.log('Please press a key to set it as your stop key.');
 		} else if (!stopKey) {
 			stopKey = event.rawcode;
-			console.log('Stop key set:', stopKey);
-			fs.writeFile(dataPath + process.env.KEYBIND_FILE, JSON.stringify({ markKey: markKey, stopKey: stopKey }), error => {
+			console.log(`Stop key set: ${stopKey}`);
+			fs.writeFile(keybindFile, JSON.stringify({ markKey: markKey, stopKey: stopKey }), error => {
 				if (error) { console.log(error); }
 			});
 		} else if (event.rawcode == markKey) {
 			const now = new Date();
 			now.setHours(now.getHours() + (now.getTimezoneOffset() / 60));
 			console.log();
-			fs.appendFile(dataPath + process.env.OUTPUT_FILE, timeInIncrements(now - streamStartTime, [24, 60, 60, 1000]) + '\n', error => {
+			fs.appendFile(outFile, timeInIncrements(now - streamStartTime, [24, 60, 60, 1000]) + '\n', error => {
 				if (error) { console.log(error); }
 			});
 		} else if (event.rawcode == stopKey) { process.exit(); }
